c2172a21c3cd69374e081d1acf2eb2a9f9f9bdc9
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2020-12-01 20:40:06 +0100

    Changes to count_big, fix concept_counts.sql

--- inst/sql/sql_server/checks/mapping_levels_drugs.sql
+++ inst/sql/sql_server/checks/mapping_levels_drugs.sql
@@ -1,9 +1,9 @@
 -- Levels at which drugs are mapped
 
 select concept_class_id as "Class",
-       count( drug_exposure_id) as "#Records",
-       count(distinct person_id) as "#Patients",
-       count(distinct drug_source_value) as "#Source Codes"
+       count_big( drug_exposure_id) as "#Records",
+       count_big(distinct person_id) as "#Patients",
+       count_big(distinct drug_source_value) as "#Source Codes"
 from @cdmDatabaseSchema.drug_exposure
 join @cdmDatabaseSchema.concept on drug_concept_id=concept_id
 group by concept_class_id
