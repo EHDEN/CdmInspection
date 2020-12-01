b5a44e4929803fd622dc9c337307cf10b16d04b3
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2020-12-01 23:21:07 +0100

    Sort STCM table

--- inst/sql/sql_server/checks/source_to_concept_map_frequency.sql
+++ inst/sql/sql_server/checks/source_to_concept_map_frequency.sql
@@ -1,3 +1,5 @@
 -- source_to_concept_map
 
-select source_vocabulary_id, target_vocabulary_id, count_big(*) as count from @cdmDatabaseSchema.source_to_concept_map group by source_vocabulary_id, target_vocabulary_id
+select source_vocabulary_id, target_vocabulary_id, count_big(*) as count from @cdmDatabaseSchema.source_to_concept_map
+group by source_vocabulary_id, target_vocabulary_id
+order by source_vocabulary_id, target_vocabulary_id
