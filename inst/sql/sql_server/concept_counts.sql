d725847e630931f037aaa1cc44814fdfa9c92260
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2020-12-01 21:38:50 +0100

    removed unused queries

--- inst/sql/sql_server/concept_counts.sql
+++
@@ -1,3 +0,0 @@
--- overview of concept table
-
-select vocabulary_id, standard_concept, count(*) from synpuf.concept group by vocabulary_id, standard_concept
--- inst/sql/sql_server/unmapped_drugs.sql
+++
@@ -1,9 +0,0 @@
---Unmapped drugs
-
-select drug_source_value as "Source Value",
-       count(drug_exposure_id) as "#Records",
-       count(distinct person_id) as "#Subjects"
-       from @cdmDatabaseSchema.drug_exposure where drug_concept_id = 0
-group by drug_source_value
-having count(drug_exposure_id)>10
-order by count(drug_exposure_id) DESC
