9557f1d58f0db75e0a64887ccd517688cf6ac0f5
Maxim Moinat <maxim@thehyve.nl>
2020-12-07 22:49:36 +0100

    non standard if empty string OR NULL

--- inst/sql/sql_server/checks/concept_counts_by_vocabulary.sql
+++ inst/sql/sql_server/checks/concept_counts_by_vocabulary.sql
@@ -5,7 +5,7 @@ select vocabulary.vocabulary_id                                  as id,
        vocabulary.vocabulary_version                             as version,
        sum(case standard_concept when 'S' then 1 else 0 end)     as num_standard,
        sum(case standard_concept when 'C' then 1 else 0 end)     as num_classification,
-       sum(case standard_concept when ''  then 1 else 0 end)     as num_non_standard
+       sum(case when standard_concept = '' OR standard_concept IS NULL  then 1 else 0 end) as num_non_standard
 from @vocabDatabaseSchema.vocabulary
 left join @vocabDatabaseSchema.concept
     on concept.vocabulary_id = vocabulary.vocabulary_id
