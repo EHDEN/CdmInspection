555087206f64f6aa88ef5d057983c28dd9955625
Maxim Moinat <maxim@thehyve.nl>
2020-12-07 17:59:52 +0100

    fix casing of Maps to relationship

--- inst/sql/sql_server/checks/performance_benchmark.sql
+++ inst/sql/sql_server/checks/performance_benchmark.sql
@@ -5,7 +5,7 @@ FROM @cdmDatabaseSchema.CONCEPT C
         JOIN @cdmDatabaseSchema.CONCEPT_RELATIONSHIP CR
                 ON C.CONCEPT_ID = CR.CONCEPT_ID_1
                 AND CR.invalid_reason IS NULL
-                AND cr.relationship_id = 'Maps To'
+                AND cr.relationship_id = 'Maps to'
         JOIN @cdmDatabaseSchema.CONCEPT C1
                 ON CR.CONCEPT_ID_2 = C1.CONCEPT_ID
                 AND C1.INVALID_REASON IS NULL
