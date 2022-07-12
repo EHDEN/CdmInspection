363000dd2550a4d83d4e807ecf6524d5d11032cc
Maxim Moinat <maxim@thehyve.nl>
2022-07-12 18:06:08 +0200

    fixes #71

--- R/Helper.R
+++ R/Helper.R
@@ -98,13 +98,18 @@ my_body_add_table <- function (x, value, style = NULL, pos = "after", header = T
 my_source_value_count_section <- function (x, data, table_number, domain, kind,smallCellCount) {
   n <- nrow(data$result)
 
-  msg <- paste0("Counts are rounded up to the nearest hundred. Values with a record count <=",smallCellCount," are omitted.")
-  if (n == 0) {
-    officer::body_add_par(x,paste0("Table ", table_number, " omitted because no ", kind, " ", domain, " were found."))
-  } else if (n < 25) {
-    officer::body_add_par(x,paste0("Table ", table_number, ". All ", n, " ", kind, " ", domain, ". ", msg))
+  if (is.null(smallCellCount)) {
+    msg <- paste0("Counts are rounded up to the nearest hundred. Values with a record count <=",smallCellCount," are omitted.")
   } else {
-    officer::body_add_par(x,paste0("Table ", table_number, ". Top 25 of ", kind, " ", domain, ". ", msg))
+    msg <- "Counts are rounded up to the nearest hundred."
+  }
+
+  if (n == 0) {
+    officer::body_add_par(x, paste0("Table ", table_number, " omitted because no ", kind, " ", domain, " were found."))
+  } else if (n < 25) {
+    officer::body_add_par(x, paste0("Table ", table_number, ". All ", n, " ", kind, " ", domain, ". ", msg))
+  } else {
+    officer::body_add_par(x, paste0("Table ", table_number, ". Top 25 of ", kind, " ", domain, ". ", msg))
   }
 
   if (n>0) {
